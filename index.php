440cfd7a9f01011fe7717dd1eca2697fc8f6bff4
Mr-C1U23 <65049629+mr-C1U23@users.noreply.github.com>
2020-08-02 15:27:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="widht=device-widht, initial-scale=1">
+		<title>Yadzz 23</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<!-- loader -->
+		<div class="bg-loader">
+			<div class="loader">
+				
+			</div>
+		</div>
+
+		<!--header-->
+		<div class="medsos">
+			<div class="container">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<header>
+			<div class="container">
+			<h1><a href="index.html">Y4DZZ__</a></h1>
+			<ul>
+				<li class="active"><a href="index.html">HOME</a></li>
+				<li><a href="about.html">ABOUT</a></li>
+				<li><a href="service.html">SERVICE</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+			</ul>
+		</div>
+		</header>
+
+		<!-- banner -->
+		<section class="banner">
+			<h2>Welcome To My Website</h2>
+		</section>
+
+		<!-- about -->
+		<section class="about"></section>
+		<center>
+			<div class="container">
+				<h3>ABOUT</h3>
+				<p>Lorem Ipsum is simply dummy text of th<strong>unknown</strong> printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the <strong>leap into electronic typesetting</strong>, remaining essentially unchanged.</p>
+			</div>
+			</center>
+
+			<!-- service -->
+			<section class="service">
+				<div class="container">
+					<h3>SERVICE</h3>
+					<div class="box">
+						<div class="col-4">
+							<a href="contact.html"><div class="icon"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+							<h4>MOBILE APP</h4>
+						</div>
+						<div class="col-4">
+							<a href="contact.html"><div class="icon"><i class="fa fa-globe" aria-hidden="true"></i></div></a>
+							<h4>WEB DEVELOPMENT</h4>
+						</div>
+						<div class="col-4">
+							<a href="contact.html"><div class="icon"><i class="fa fa-edit" aria-hidden="true"></i></div></a>
+							<h4>DESIGN</h4>
+						</div>
+						<div class="col-4">
+							<a href="contact.html"><div class="icon"><i class="fa fa-bar-chart" aria-hidden="true"></i></div></a>
+							<h4>DIGITAL MARKETING</h4>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- footer -->
+			<footer>
+				<div class="container">
+					<small>Copyright &copy; 2020 - YADZZ.</small>
+				</div>
+			</footer>
+
+
+			<script type="text/javascript">
+				$(document).ready(function(){
+					$(".bg-loader").hide();
+				})
+			</script>
+	</body>
+</html>
